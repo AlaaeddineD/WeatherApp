27d08cf8ac9a429ec8ffdaaaf5e3b819de157e3a
Alaaeddine <dhifallah.alaaeddine@gmail.com>
2023-05-14 23:41:25 +0200

    Verifier la disponibilité d'Internet

--- WeatherApp/ViewController/HomeViewController.swift
+++ WeatherApp/ViewController/HomeViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        NetworkMonitor.shared.startMonitoring()
+    }
+    
+    private func checkInternetAvailability() -> Bool {
+        if NetworkMonitor.shared.isInternetAvailable(){
+            return true
+        }
+        
+        // Create an alert
+        var dialogMessage = UIAlertController(title: "Attention", message: "une connexion Internet est requise ! veuillez vérifier votre connexion et réessayer", preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "OK", style: .default)
+        dialogMessage.addAction(ok)
+        self.present(dialogMessage, animated: true, completion: nil)
+        
+        return false
     }
     
     //Button action, pour présenter la vue météo
     @IBAction func presentWeatherView(){
+        
+        //Empêcher l'utilisateur d'ouvrir la vue météo sans Internet
+        if !checkInternetAvailability(){
+            return
+        }
+        
         guard let weatherView = storyboard?.instantiateViewController(withIdentifier: WeatherViewController.storyboardIdentifier) as? WeatherViewController else {
             
             print("Impossible de trouver une vue avec cet identifiant \(WeatherViewController.storyboardIdentifier)")
