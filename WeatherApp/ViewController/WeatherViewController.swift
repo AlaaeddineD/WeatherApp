d251e722bd24ba22fb8262e7a43e29056c70858a
Alaaeddine <dhifallah.alaaeddine@gmail.com>
2023-05-14 20:07:01 +0200

    Weather View: UI et Binding
    le UI de "Weather View" et Binding avec le view model

--- WeatherApp/ViewController/WeatherViewController.swift
+++ WeatherApp/ViewController/WeatherViewController.swift
@@ -7,14 +7,29 @@
 
 import UIKit
 
+protocol WeatherViewControllerProtocol {
+    
+}
+
 class WeatherViewController: UIViewController {
     
     static let storyboardIdentifier = "WeatherView"
     
+    //UI
+    @IBOutlet weak var weatherTableView: UITableView!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let viewModel = WeatherViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        viewModel.delegate = self
     }
+    
+}
+
+//MARK: VM Binding methods
+extension WeatherViewController: WeatherViewControllerProtocol{
     
 }
---
+++ WeatherApp/ViewModels/WeatherViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherViewModel.swift
+//  WeatherApp
+//
+//  Created by WG CONSULTING on 14/05/2023.
+//
+
+import Foundation
+
+class WeatherViewModel{
+    
+    var delegate: WeatherViewControllerProtocol?
+}
